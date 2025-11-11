c268015f66547a613c8481d7083e340e2fb45ffc
Suhail-22 <101893675+Suhail-22@users.noreply.github.com>
2025-11-11 11:02:10 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+// Inlined Service Worker
+const CACHE_NAME = 'ai-calculator-cache-v8';
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
